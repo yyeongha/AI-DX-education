2d9cfbd67e23299630114783a8da9ec5db32752d
Hayeong Yang <yha0719@naver.com>
2024-05-02 13:39:48 +0900

    변경사항 커밋

--- 010_class.py
+++ 010_class.py
@@ -1,8 +1,13 @@
-class Cafe:
-    def ordering(self):
-        print("주문받아요!")
+# 초기화할때 "커피명"
+# ordering "컵사이즈"
+from cafe import *
 
-nobrand = Cafe()
-nobrand.ordering()
+starbucks = Cafe("스타벅스", "카페라떼")
+twosome = Cafe("투썸","아메리카노")
+ediya = Cafe("이디야","바닐라 라떼")
+
+starbucks.ordering("톨")
+twosome.ordering("레귤러")
+ediya.ordering("빅")
 
 
